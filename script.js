593261e18ba752238fbd9598f7dcf910f124e473
PankajThate <124923727+PankajThate@users.noreply.github.com>
2023-06-30 14:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const byteSize = (str) => {
   // write your code here
+	if(str===""){
+		return 0;
+		
+	}else {
+		return str.length;
+	}
 };
+
+
 
 // Do not change the code below
 const str = prompt("Enter some string.");
